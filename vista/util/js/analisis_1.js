3ca55e198e2f0f196b4bde0d9e89342b735e9cf4
pinformatica <pinformatica@ocastro.CeramicaItalia.com>
2016-11-21 12:24:39 -0500

    Corrección en CISA

--- vista/util/js/analisis_1.js
+++ vista/util/js/analisis_1.js
@@ -4,18 +4,17 @@
  * and open the template in the editor.
  */
 
-$('#formTraz').submit(function (event) {
-    //Se establece un rango de fechas    
-    var fechaIni = $('#fechaInicio').val();
-    var fechaFin = $('#fechaFin').val();
-    //Condicional para validar que las fechas sean validas
-    if (fechaFin < fechaIni) {
-        //Se corta el evento del submit
-        event.preventDefault();
-        $('#fechaFin').focus();
-        toastr["error"]('Fecha de finalización mayor que la fecha de inicio');
-    }
+
+$(document).ready(function () {
+    //Se visualiza el formato
+    verFormato('analizar');
 });
+
+$("#btnExportTablaGeneral").click(function (e) {
+    window.open('data:application/vnd.ms-excel,' + $('#res1').html());
+    e.preventDefault();
+});
+
 
 var datos = new Array();
 
@@ -102,7 +101,6 @@ $('#visualizarFormato').on('click', 'input[type="button"],select', function () {
     var label = input.parent('div').children('label').text();
     var id = validarNombreTitulo(label);
     var clave = input.attr('name');
-    //Titulo que se muestra para saber que analisis se usa, y demás creaciones primitivas de la interfaz
     var titulo = '<h3>Se ha seleccionado ' + label + ' para su análisis</h3><br>\n\
                                         <h4>Registros dentro del rango de fechas</h4><br>';
     $('#resultado').append(titulo);
@@ -110,13 +108,9 @@ $('#visualizarFormato').on('click', 'input[type="button"],select', function () {
     var tabla = '<table id="' + id + '" class="table-bordered table-hover"><thead><tr><th>Fecha del registro</th><th>Valor del registro</th></tr></thead><tbody></tbody></table>';
     $('#resultado').append(tabla);
     $('#res1').append('<table><thead><tr><th>Fecha del registro</th><th>Valor del registro</th></tr></thead><tbody></tbody></table>');
-//Recorrido de los datos (registros del formato)
     for (var index in datos) {
         arregloInfo[index] = new Array(datos[index][0], datos[index][1][clave]);
-        //creacion de las filas
         var fila = '<tr><td>' + datos[index][0] + '</td><td>' + datos[index][1][clave] + '</td></tr>';
-        //creacion de los campos clave para la fila
-        fila += cuerpoCamposClave(index) + '</tr>';
         $('#' + id + ' tbody').append(fila);
         $('#res1 table tbody').append(fila);
     }
@@ -128,88 +122,39 @@ $('#visualizarFormato').on('click', 'input[type="button"],select', function () {
     $('#res1 table').tablesorter({sortList: [[0, 1]]});
 });
 
-/**
- * Método que devuelve columnas que representan los campos clave del formato
- * @param {type} tipo tipo de columna que se va a pintar td o th
- * @returns {unresolved}
- */
-function encabezadoCamposClave(tipo) {
-    var camposClave = sessionStorage.getItem('camposClave');
-    var cc = camposClave.split('-');
-    var campo = '';
-    var enc;
-    for (campo in cc) {
-        if (cc[campo] !== '') {
-            enc += '<' + tipo + '>' + cc[campo] + '</' + tipo + '>';
-        }
-    }
-    return enc;
-}
-
-/**
- * Método que llena las filas con sus campos clave del registro
- * @param {type} index
- * @returns {unresolved}
- */
-function cuerpoCamposClave(index) {
-    var camposClave = sessionStorage.getItem('camposClave');
-    var cc = camposClave.split('-');
-    var campo = '';
-    var col;
-    for (campo in cc) {
-        var clave = cc[campo];
-        if (clave !== '') {
-            col += '<td>' + datos[index][1][clave] + '</td>';
-        }
-    }
-    return col;
-}
-
 function mostrarFormato() {
     $('#visualizarFormato').show();
     $('#btnExportTablaGeneral').hide();
     $('#resultado').hide();
-    $('#res1').hide();
-    $('#res2').hide();
     $('#chart-container').hide();
     $('#verFormato').hide();
 }
 
-/**
- * Método que al hacer clic sobre una tabla del formato muestra los datos del registro
- */
+
 $('#visualizarFormato').on('click', 'table td', function () {
     $('#res2').html('');
-    //columna actual a graficar especificamente
     var columna = $(this);
-    //Tabla a la que pertenece la columna
     var tabla = columna.parents('table');
     var tabla_name = tabla.attr('id');
     var col0 = $('#' + tabla_name + ' tr td:eq(0)').text();
     var col1 = columna.text();
-    //se validan el nombre de la columna
     var texto = validarNombreTitulo(col1);
-    //posicion de la columna cliqueada
     var index = columna.index();
 
 
-// se recorre todas las filas de la tabla del formato
+
     $('#' + tabla.attr('id') + ' tr').each(function (i) {
         if (i === 0) {
 //            var msj = '<table id="pintar_' + tabla_name + '" class="table-bordered table-hover"><thead><tr><td>' + col0 + '</td><td>' + col1 + '</td><td>Fecha del registro</td></tr></thead><tbody></tbody></table>';
             var msj = '<table id="pintar_' + tabla_name + '" class="table-bordered table-hover"><thead><tr><td>' + col0 + '</td><td>' + col1 + '</td></tr></thead><tbody></tbody></table>';
             $('#res2').append(msj);
-            //se buscan los campos clave
-            var camposClave = encabezadoCamposClave('td');
-            $('#res2 thead tr').append(camposClave);
         }
         var fila = $(this);
-        //se recorren los registros.
         for (var d in datos) {
             var flag = false;
             var row = '<tr>';
 //            var row = '<tr><td>' + (datos[d][0]) + '</td>';
-//se recorre cada columna de la tabla del formato
+
             $(fila).children('td').each(function () {
                 var td = $(this);
                 var index2 = td.index();
@@ -219,29 +164,21 @@ $('#visualizarFormato').on('click', 'table td', function () {
                     row += '<td>' + col + '</td>';
                 }
                 else {
-                    //si la columna cliqueada es igual a cero o igual a la posicion de la columna actual
-                    if (index2 === index || index2 === 0) {
+                    if (index2 === index || index2 === 0) {                        
                         var input = td.children('input').attr('name');
-                        //Se busca en el reigstro de la siguiente forma: 
-                        //donde d= es la posicion actual del registro.
-                        //1 corresponde a la casilla de la información, arreglo asociativo
-                        //input corresponde a la propiedad del arreglo asociativo que es la clave.
                         var valor = (datos[d][1][input]);
-                        //si el valor es vacio no se guarda
                         if (valor !== undefined) {
                             flag = true;
                         }
                         else {
                             valor = '';
                         }
-                        if (index2 === 0) {
-                            valor += '<br>' + (datos[d][0]);
+                        if(index2===0){
+                            valor+='<br>'+(datos[d][0]);
                         }
-                        //se crea la columna 
                         row += '<td>' + valor + '</td > ';
 
                         var x = td.children().first();
-                        //si la primera columna tiene valores de entrada o seleccionables se toma el valor siempre y cuando no sea vacio
                         if (x.is('input')) {
                             var input = x.attr('name');
                             if (input.includes(texto)) {
@@ -263,9 +200,7 @@ $('#visualizarFormato').on('click', 'table td', function () {
 
                 }
             });
-            //se buscan los demas valores de los campos clave a traves del indice d que es la posicion de ese registro en los datos
             row += '</tr>';
-            //si tiene al menos una columna no vacia se almacena la fila
             if (flag) {
                 $('#pintar_' + tabla_name + ' tbody').append(row);
             }
@@ -300,12 +235,9 @@ function tablaCompleta(tabla_name) {
     var msj = "";
     var nombreTabla = '';
 
-    //se recorre la tabla para crear la tabla general perteneciente a ese registro
     $(tabla).each(function (i) {
         var fila = $(this);
-        //si la tabla no tiene input es porque es un encabezado de la tabla
         if (!fila.has('input').length) {
-            //se crea la tabla y el encabezado
             nombreTabla = 'fila' + i;
             msj = '<table id="' + nombreTabla + '" class="table-bordered table-hover">\n\
                     <thead></thead><tbody></tbody></table>';
@@ -313,7 +245,6 @@ function tablaCompleta(tabla_name) {
             $('#res1').append('<table><thead></thead><tbody></tbody></table>');
 //            var encabezado = '<tr><td>Fecha sistema</td>';
             var encabezado = '<tr><th>Fecha del registro</th>';
-            //se recorre cada columna para que sea una columna de encabezado 
             $(fila).children('td').each(function () {
                 var td = $(this);
                 var col = td.text();
@@ -321,7 +252,6 @@ function tablaCompleta(tabla_name) {
                     encabezado += '<th>' + col + '</th>';
                 }
             });
-            //se agregan los encabezados a la tabla junto con los campos clave
             encabezado += '</tr>';
             $('#fila' + i + ' thead').append(encabezado);
             $('#res1 table thead').append(encabezado);
@@ -332,25 +262,21 @@ function tablaCompleta(tabla_name) {
         else {
 //            console.log('fila: ' + i);
 //            console.log(datos);
-//se recorren los registros            
+
             for (var d in datos) {
 //                console.log(d);
-                //se toma la fecha del sistema del registro
                 var flag = false;
                 var row = '<tr><td>' + (datos[d][0]) + '</td>';
 //                var row = '<tr>';
 //                console.log(datos[d][1]);
-                //Se recorre las demas columnas
                 $(fila).children('td').each(function () {
-                    //se toma la columna y si es vacio se pone igual
-                     td = $(this);
+                    var td = $(this);
                     var col = td.text();
 //                    console.log(td.html());
                     if (col !== '') {
                         row += '<td>' + col + '</td>';
                     }
                     else {
-                        //Se pone el valor de la entrada.
                         var input = td.children('input').attr('name');
 //                        console.log(input);
                         var valor = (datos[d][1][input]);
@@ -362,8 +288,7 @@ function tablaCompleta(tabla_name) {
                         else {
                             valor = '';
                         }
-                        //se buscan los valores de los campos clave
-                row += '<td>' + valor + '</td > ';
+                        row += '<td>' + valor + '</td > ';
                     }
                 });
                 row += '</tr>';
@@ -383,76 +308,11 @@ function tablaCompleta(tabla_name) {
 ;
 //});
 
-/**
- * método que muestra la trazabilidad de todos los registros de los datos básicos. primero se crea el encabezado
- * @returns {undefined}
- */
-function trazabilidadCompleta() {
-    //validaciones.
-    $('#ver').hide();
-    $('btnExportTablaGeneral').show();
-    $('input[type="checkbox"]').attr('checked', true);
-    $('input[type="radio"]').attr('checked', true);
-    $('#visualizarFormato table').remove();
-    //se buscan todos los campos
-    var cad = $('#visualizarFormato').serialize();
-    console.log(cad);
-    var campos = cad.split('&');
-    var i = '';
-    $('#divRes').append('<table><tbody></tbody></table');
-    var encabezadoResultado = '<tr><th>Fecha de sistema</th>';
-    //se crean las columnas segun los campos
-    for (i in campos) {
-        var campo = campos[i].split('=');
-        var clave = campo[0];
-        encabezadoResultado += '<th>' + clave + '</th>';
-    }
-    encabezadoResultado += '</tr>';
-    $('#tableRes thead').append(encabezadoResultado);    
-    $('#divRes table tbody').append(encabezadoResultado);    
-    llenarCuerpoTablaTrazabilidadCompleta();
-    acomodarTabla('tableRes');
-    $('#divRes table').tablesorter({sortList: [[0, 1]]});
-}
-
-/**
- * se llena el cuerpo de la tabla
- * @returns {undefined}
- */
-function llenarCuerpoTablaTrazabilidadCompleta() {
-    //se recorre los datos primero
-    for (var i in datos) {
-        var fila = '<tr><td>' + datos[i][0] + '</td>';
-        //se recorren las columnas para obtener los datos.
-        $('#tableRes thead tr th').each(function (j) {
-            if (j !== 0) {
-                //se busca el valor segun la columna
-                var clave = $(this).text();
-                var valor = datos[i][1][clave];
-                console.log(valor);
-                //si no tiene valor no se pone
-                if (valor===undefined) {
-                    valor = '';
-                }
-                var col = '<td>' + valor + '</td>';
-                fila += col;
-            }
-        });
-        fila += '</tr>';
-        //Se completan las tablas
-        $('#tableRes tbody').append(fila);
-        $('#divRes table tbody').append(fila);
-    }
-
-}
 
 
 
-/**
- * Método que reemplaza el nombre del titulo de una tabla
- * @param {type} titulo
- * @returns {unresolved}
- */
+
+
 function validarNombreTitulo(titulo) {
     titulo = titulo.toLowerCase();
     titulo = titulo.replace(/[áàäâå]/g, 'a');
@@ -473,10 +333,7 @@ function validarNombreTitulo(titulo) {
     return titulo;
 }
 
-/**
- * Método que pinta la gráfica específica de datos
- * @returns {undefined}
- */
+
 function pintarGrafica() {
     $('#res2 table').each(function (i) {
         var id = "tabla_" + i;
@@ -502,11 +359,6 @@ function pintarGrafica() {
     });
 }
 
-/**
- * Método que acomoda una tabla para paginar
- * @param {type} tabla
- * @returns {undefined}
- */
 function acomodarTabla(tabla) {
     $('#' + tabla).DataTable({
         responsive: true,
@@ -533,12 +385,7 @@ function acomodarTabla(tabla) {
     });
 }
 
-/**
- * método que pinta una grafica de gantt
- * @param {type} titulo
- * @param {type} info
- * @returns {undefined}
- */
+
 function timeLine(titulo, info) {
     var fechaIni = $('#fechaInicio').val();
     var fechaFin = $('#fechaFin').val();
